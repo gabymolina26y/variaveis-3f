910db09de13a87f95a34cbb8b3c568ceced18228
gabymolina26y <116806981+gabymolina26y@users.noreply.github.com>
2024-08-12 23:10:19 +0000

    12/08/24/2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const idade = 16;
+let idade = 16;
 const anoNascimento = 2007;
 let anoAtual = 2024;
+const nomeCompleto = "Gabryelle DAL Lin";
+
+console.log("Meu nome é " + nomeCompleto + " nascida no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
 
 anoAtual = anoAtual + 1;
-
-
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade);
